2edcf255ecfce37201f68c92baf79dd4e595da82
Nour <noureldin@gmail.com>
2025-01-10 23:11:59 +0200

    update

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,23 +1,32 @@
 var cart = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
   : [];
-
-var cart_items = document.getElementById("cart-items");
-if (cart_items) {
-  if (cart.length === 0) {
-    cart_items.innerHTML =
-      "<tr class='text-center'><td colspan='4'>No items in cart</td></tr>";
-  } else {
-    cart.forEach((item) => {
-      cartRow = document.createElement("tr");
-      cartRow.innerHTML = `<td>${item.name}</td>
+showCart();
+function showCart() {
+  var cart_items = document.getElementById("cart-items");
+  cart_items.innerHTML = "";
+  if (cart_items) {
+    if (cart.length === 0) {
+      cart_items.innerHTML =
+        "<tr class='text-center'><td colspan='4'>No items in cart</td></tr>";
+    } else {
+      cart.forEach((item) => {
+        cartRow = document.createElement("tr");
+        cartRow.innerHTML = `<td class="rounded-start-5" >${item.name}</td>
                                         <td>${item.price}</td>
-                                        <td>${item.qty}</td>
+                                      <td>  
+                                       <div class="cart-operators">
+                                         <i class="fa-solid fa-minus text-danger"></i>
+                                          ${item.qty}
+                                         <i class="fa-solid fa-plus text-success"></i>
+                                       </div>
+                                      </td>
+                                      </td>
                                         <td>${item.total}</td>
-                                        <td><i class="fa-solid fa-trash text-danger"></i></td>`;
-
-      cart_items.appendChild(cartRow);
-    });
+                                        <td class="rounded-end-5"><i id="trash" class="fa-solid fa-trash text-danger " onclick="deleteProductToCart('${item.item_id}')"></i></td>`;
+        cart_items.appendChild(cartRow);
+      });
+    }
   }
 }
 getCartDetails();
@@ -38,12 +47,13 @@ function addProductToCart(
   var getItem = cart.find((i) => {
     return i.item_id === item_id;
   });
-
   if (getItem) {
     // update qty
-    console.log("item exist");
+    getItem.qty =
+      operation === "increase" ? getItem.qty + qty : getItem.qty - qty;
+    getItem.total = getItem.qty * getItem.price;
+    localStorage.setItem("cart", JSON.stringify(cart));
   } else {
-    // new item in cart
     var cartObj = {
       item_id,
       name: name,
@@ -57,4 +67,15 @@ function addProductToCart(
   // to update the cart count & cart items
   getCartDetails();
 }
-function deleteProductToCart() {}
+function deleteProductToCart(item_id) {
+  cart = cart.filter((item) => item.item_id != item_id);
+
+  // var item = cart.find((i) => {
+  //   return i.item_id === item_id;
+  // });
+
+  // cart.splice(cart.indexOf(item), 1);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  showCart();
+  getCartDetails();
+}
