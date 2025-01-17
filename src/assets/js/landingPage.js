4231d34dcbb8b0ac5c192332d4c9843d71e8403b
Mohamed <m.mamdouh1462@gmail.com>
2025-01-17 18:59:35 +0200

    add NavBar By Mohamed

--- src/assets/js/landingPage.js
+++ src/assets/js/landingPage.js
@@ -18,3 +18,24 @@ scrollerContent.forEach((item) => {
   duplicatedItem.setAttribute("aria-hidden", true);
   scrollerInner.appendChild(duplicatedItem);
 });
+
+const fixedNav = document.querySelector("nav.fixed");
+let lastScrollPosition = 0;
+
+window.addEventListener("scroll", function () {
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 80) {
+    if (scrollPosition > lastScrollPosition) {
+      // Scrolling down
+      fixedNav.classList.add("active");
+    } else {
+      // Scrolling up
+      fixedNav.classList.remove("active");
+    }
+  } else {
+    fixedNav.classList.remove("active");
+  }
+
+  lastScrollPosition = scrollPosition;
+});
