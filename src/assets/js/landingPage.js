9ea7db09efc6808a12c2b860d15607b4666dad10
Mohamed <m.mamdouh1462@gmail.com>
2025-01-17 21:23:34 +0200

    last Part in NavBar insha2allah

--- src/assets/js/landingPage.js
+++ src/assets/js/landingPage.js
@@ -20,13 +20,45 @@ scrollerContent.forEach((item) => {
 });
 
 const fixedNav = document.querySelector("nav.fixed");
+let burgerIcons = document.querySelectorAll(".burger-icon");
+let mobileLinks = document.querySelector(".mobile-links");
 let lastScrollPosition = 0;
+let mobileLinksIsActive = false;
+let mobileWindow = false;
 
-window.addEventListener("scroll", function () {
+// Check the window width on page load
+checkWindowWidth();
+
+// Check the window width on window resize
+window.addEventListener("resize", checkWindowWidth);
+
+window.addEventListener("scroll", handleScroll);
+
+burgerIcons.forEach((item) => {
+  item.addEventListener("click", toggleMobileLinks);
+});
+
+function checkWindowWidth() {
+  const windowWidth = window.innerWidth;
+  if (windowWidth > 768) {
+    mobileWindow = false;
+    mobileLinks.classList.remove("fixed");
+    mobileLinks.classList.add("hidden");
+    burgerIcons.forEach((e) => {
+      e.classList.remove("active");
+    });
+    mobileLinksIsActive = false;
+    window.addEventListener("scroll", handleScroll);
+  } else {
+    mobileWindow = true;
+  }
+}
+
+function handleScroll() {
   const scrollPosition = window.scrollY;
 
   if (scrollPosition > 80) {
-    if (scrollPosition > lastScrollPosition) {
+    if (scrollPosition < lastScrollPosition) {
       // Scrolling down
       fixedNav.classList.add("active");
     } else {
@@ -38,4 +70,42 @@ window.addEventListener("scroll", function () {
   }
 
   lastScrollPosition = scrollPosition;
-});
+}
+
+function checkScroll() {
+  const scrollPosition = window.scrollY;
+  if (scrollPosition > 80) {
+    fixedNav.classList.add("active");
+  } else {
+    fixedNav.classList.remove("active");
+  }
+}
+
+function toggleMobileLinks() {
+  if (mobileWindow) {
+    mobileLinksIsActive = !mobileLinksIsActive;
+    console.log(mobileLinksIsActive);
+    burgerIcons.forEach((e) => {
+      e.classList.toggle("active");
+    });
+    if (mobileLinksIsActive) {
+      fixedNav.classList.add("active");
+      mobileLinks.classList.add("fixed");
+      mobileLinks.classList.remove("hidden");
+      window.removeEventListener("scroll", handleScroll);
+    } else {
+      fixedNav.classList.remove("active");
+      mobileLinks.classList.remove("fixed");
+      mobileLinks.classList.add("hidden");
+      window.addEventListener("scroll", handleScroll);
+    }
+    checkScroll();
+  } else {
+    mobileLinks.classList.remove("fixed");
+    mobileLinks.classList.add("hidden");
+    burgerIcons.forEach((e) => {
+      e.classList.remove("active");
+    });
+    checkScroll();
+  }
+}
