e79c13f344a06a31cae28fe2f5493f83795f7c76
Kirellos Shehata <58255421+KiroCarllos@users.noreply.github.com>
2025-01-18 22:15:39 +0200

    solve ' issue when add product to cart

--- src/assets/js/products.js
+++ src/assets/js/products.js
@@ -63,7 +63,7 @@ function showProducts(products) {
       <span class="text-2xl font-bold text-gray-900">$${p.price}</span>
       <button class="rounded-full transform transition-all duration-500 hover:scale-105 border-2 border-gray-300 bg-gray-900 px-5 py-2.5 text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-900" onclick="addProductToCart('${
         p.id
-      }','${p.title}','${p.price}')">
+      }','${p.title.replace(/'/g, "\\'")}','${p.price}')">
       <svg xmlns="http://www.w3.org/2000/svg" class=" h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
       <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
       </svg>
